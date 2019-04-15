dc6a97dbc017718e328487f180f1bee235d0b54f
unknown <you@example.com>
2019-04-15 17:54:06 +0800

    版本1.01

---
+++ tantan/src/components/Xheader.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+     <header>
+    <!-- 左图标 -->
+    <div class="leftIcon"></div>
+
+    <!-- 标题 -->
+    <div class="title">新手指导</div>
+    <!-- 右图标 -->
+    <div class="rightIcon"></div>
+  </header>
+ 
+</div>
+ 
+</template>
+<script>
+export default {};
+</script>
+<style scoped >
+/* css是局部样式 */
+header {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: white;
+  background-color: red;
+  /* 弹性盒布局 */
+  display: flex;
+}
+.leftIcon {
+  background:url(../assets/menu.png) no-repeat center ;
+  background-size: 24px;
+
+    flex: 1;
+}
+
+.title {
+  flex: 3;
+}
+
+.emptyIcon {
+  flex: 1;
+}
+ .rightIcon {
+      background:url(../assets/my.png) no-repeat center ;
+     background-size: 24px;
+    flex: 1;
+  }
+ 
+
+</style>
---
+++ tantan/src/lei/Lmain.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <xheader></xheader>
+    <div class="img">
+      <img src="../assets/nav.png" alt>
+    </div>
+    <div>
+      <ul class="mail">
+        <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+         <li >
+          <span class="lift">探探怎么玩</span>
+          <span class="right"></span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import xheader from "../components/Xheader.vue";
+export default {
+  components: {
+    xheader
+  }
+};
+</script>
+<style scoped>
+.img img {
+  width: 100%;
+}
+.mail li {
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid rgb(241, 245, 244);
+  line-height: 60px;
+  position: relative;
+}
+.lift{
+   position: absolute;
+  left: 20px;
+}
+.right {
+  background: url(../assets/arrow-right.png) no-repeat center;
+  background-size: 20px;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 10px;
+  top: 19px;
+}
+</style>
+
+
